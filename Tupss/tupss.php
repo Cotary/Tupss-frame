b7f5a042ead2a1b831ceeb2c14a1a327a34b330f
cotary <1105629800@qq.com>
2019-10-14 23:11:16 +0800

    --

--- Tupss/tupss.php
+++ Tupss/tupss.php
@@ -1,1 +1,32 @@
 <?php
+
+final class Tupss
+{
+    public static function run()
+    {
+        self::_set_const();   //设置常量
+        self::_create_dir();   //创建应用文件夹
+        self::_import_file();  //载入所需文件
+        Application::run();
+    }
+
+    private static function _set_const()
+    {
+        //后面都有 /
+        $rootPath = str_replace('\\', '/', __FILE__);
+        define('TUPSS_PATH', dirname($rootPath));
+        define('CONFIG_PATH', TUPSS_PATH . '/config');
+        define('DATA_PATH', TUPSS_PATH . '/data');
+        define('LIB_PATH', TUPSS_PATH . '/lib');
+        define('CORE_PATH', LIB_PATH . '/core');
+        define('FUNCTION_PATH', LIB_PATH . '/function');
+        define('ROOT_PATH', dirname(TUPSS_PATH));
+        //应用目录
+        define('APP_PATH', ROOT_PATH.'/'.APP_NAME);
+        echo ROOT_PATH;
+    }
+
+
+}
+
+Tupss::run();
--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php
+define('APP_NAME','index');
 require './Tupss/tupss.php';
