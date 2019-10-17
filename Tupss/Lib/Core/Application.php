56076a147d12caca35fabbb25acffbd190428381
cotary <1105629800@qq.com>
2019-10-17 10:13:29 +0800

    --

--- Tupss/Lib/Core/Application.php
+++ Tupss/Lib/Core/Application.php
@@ -11,10 +11,14 @@ final class Application
 {
     public static function run()
     {
-        self::init();
+        self::_init();
+        self::_set_url();
+        spl_autoload_register(array(__CLASS__, '_autoload'));//自动载入
+        self::_create_demo();
+        self::_app_run();
     }
 
-    private static function init()
+    private static function _init()
     {
         //加载配置项（用户配置的优先级更高）
         C(require CONFIG_PATH . '/config.php');
@@ -35,4 +39,48 @@ EOF;
         C('SESSION_AUTO_START') && session_start();
     }
 
+    private static function _set_url()
+    {
+        // P($_SERVER);
+        $path = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
+        $path = str_replace('\\', '/', $path);
+        define('__APP__', $path);
+        define('__ROOT__', dirname(__APP__));
+        define('__TPL__', __ROOT__ . '/' . APP_NAME . '/Tpl');
+        define('__PUBLIC__', __TPL__ . '/Public');
+
+    }
+
+    /**
+     * @param $className 自动载入
+     */
+    private static function _autoload($className)
+    {
+
+    }
+
+    /**
+     * 创建默认控制器
+     */
+    private static function _create_demo()
+    {
+        $path = APP_CONTROLLER_PATH . '/IndexController.class.php';
+        $str = <<<EOF
+<?php
+
+class IndexController
+{
+    public function index()
+    {
+        echo 'Welcome To Tupss!';
+    }
+}
+EOF;
+        is_file($path) || file_put_contents($path, $str);
+    }
+
+    private static function _app_run()
+    {
+
+    }
 }
